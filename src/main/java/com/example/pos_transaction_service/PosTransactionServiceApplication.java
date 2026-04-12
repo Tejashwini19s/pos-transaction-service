a46d195b83e3bdb107e3000a4d8db7d1bf8b8b7e
Tejashwini S <Tejashwini S@192.168.41.74>
2026-04-12 12:53:25 +0530

    Initial commit: POS Transaction Service setup

---
+++ src/main/java/com/example/pos_transaction_service/PosTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pos_transaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pos_transaction_service/PosTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pos_transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
